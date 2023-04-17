33de610380ade86912f6281565fcd2a188c2d094
miten <57693631+m1ten@users.noreply.github.com>
2023-04-17 14:47:23 -0400

    fmt

--- src/findtarget.py
+++ src/findtarget.py
@@ -4,6 +4,7 @@ import glob
 import sys
 import re
 
+
 class Image:
     name: str
     distance: float
@@ -14,13 +15,16 @@ class Image:
         self.distance = distance
         self.angle = angle
 
+
 def focal_length(measured_distance, real_width, width_in_rf_image):
     focal_length_value = (width_in_rf_image * measured_distance) / real_width
     return focal_length_value
 
+
 def distance_finder(focal_length, real_face_width, face_width_in_frame):
     distance = (real_face_width * focal_length)/face_width_in_frame
     return distance
+
 
 def main(fname: str, display: bool):
 
@@ -28,7 +32,7 @@ def main(fname: str, display: bool):
     # fls = []
 
     # print(f'\n{fname}')
-    img = cv.imread(fname) 
+    img = cv.imread(fname)
 
     # circle fiducials
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -46,14 +50,15 @@ def main(fname: str, display: bool):
     # res = cv.bitwise_and(img, img, mask=mask)
 
     gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
-        
+
     # filter out noise, there are 4 fiducials +/- 10% of each other
-    kernel = np.ones((5,5),np.uint8)
+    kernel = np.ones((5, 5), np.uint8)
     mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
     mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
-        
+
     # find contours
-    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(
+        mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # find the 4 fiducials
     fiducials = []
@@ -70,7 +75,7 @@ def main(fname: str, display: bool):
 
     # show the fiducials
     for fiducial in fiducials:
-        cv.circle(img, (fiducial[0], fiducial[1]), 5, (0,0,255), -1)
+        cv.circle(img, (fiducial[0], fiducial[1]), 5, (0, 0, 255), -1)
 
     # find the center of the fiducials, which is the target
     target = [0, 0]
@@ -81,40 +86,41 @@ def main(fname: str, display: bool):
     target[1] = int(target[1] / len(fiducials))
 
     # show the target
-    cv.circle(img, (target[0], target[1]), 5, (0,255,0), -1)
-        
+    cv.circle(img, (target[0], target[1]), 5, (0, 255, 0), -1)
+
     # show the image
     if display:
         # resize the image by 50% to fit
-        show_img = cv.resize(img, (0,0), fx=0.5, fy=0.5)
+        show_img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
         cv.imshow(f'{fname} modified', show_img)
         cv.waitKey(0)
-        cv.destroyAllWindows()    
+        cv.destroyAllWindows()
 
     # distance from top left fiducial to bottom right fiducial (center to center) in pixels
-    fid_dis_left_to_right_pixels = np.sqrt((fiducials[0][0] - fiducials[3][0])**2 + (fiducials[0][1] - fiducials[3][1])**2)
+    fid_dis_left_to_right_pixels = np.sqrt(
+        (fiducials[0][0] - fiducials[3][0])**2 + (fiducials[0][1] - fiducials[3][1])**2)
 
     # print('fid_dis_left_to_right_pixels = ', fid_dis_left_to_right_pixels)
 
     # real = reals[0] * 1000
 
     # fl = focal_length(real, fid_dis_left_to_right, fid_dis_left_to_right_pixels)
-    # fls.append((real, fl))           
+    # fls.append((real, fl))
 
-    found_distance = distance_finder(avg_fl, fid_dis_left_to_right, fid_dis_left_to_right_pixels)
+    found_distance = distance_finder(
+        avg_fl, fid_dis_left_to_right, fid_dis_left_to_right_pixels)
 
     # print(f"Real distance: {real}mm")
     # print(f"Found distance: {found_distance} mm")
 
     # determine the angle of the target
-    center_of_image = img.shape[1] / 2 
+    center_of_image = img.shape[1] / 2
     angle = np.arctan((target[0] - center_of_image) / avg_fl)
     # convert to degrees
     angle = angle * 180 / np.pi
     # print(f"Angle: {angle} degrees")
 
     return Image(fname, found_distance, angle)
-
 
     # reals.pop(0)
 
@@ -124,6 +130,7 @@ def main(fname: str, display: bool):
 #     raw_fls.append(fl)
 # average_fl = sum(raw_fls) / len(raw_fls)
 # print(f"Average focal length: {average_fl}")
+
 
 if __name__ == '__main__':
     dirt = 'delta/*mm.jpg'
@@ -145,11 +152,11 @@ if __name__ == '__main__':
     images = glob.glob(args[0])
 
     # distance from top left fiducial to bottom right fiducial (center to center) in mm
-    fid_dis_left_to_right = 172 # 172 millimeters
-    fid_diameter = 69 # 69 millimeters
+    fid_dis_left_to_right = 172  # 172 millimeters
+    fid_diameter = 69  # 69 millimeters
 
     # this is only true for images in delta/
-    avg_fl = 1695.6 # 1695.6 millimeters
+    avg_fl = 1695.6  # 1695.6 millimeters
 
     imgs = []
     for image in images:
